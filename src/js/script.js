9e86847c7541830bfc31b40a92b576d9840ad577
Ricardo Salmerón <ricardosalmeronps@gmail.com>
2026-08-13 11:35:59 -0300

    [Chore] Projeto q falhou em um script

---
+++ src/js/script.js
@@ -0,0 +1,41 @@
+setTimeout(()=>{
+    console.log("Processo assíncorono")
+}, 2000);
+
+console.log("1-inicio do processo");
+setTimeout(()=>{
+
+    console.log("2-meio do processo");
+}, 1000);
+
+
+
+console.log("3-fim do processo");
+
+const btn = document.getElementById('botao');
+const popup = document.getElementById('popup');
+
+btn.addEventListener('click', () => {
+    popup.classList.add('popup-active')
+
+    
+    setTimeout(() => {
+        console.log('chegou aqui!')
+        popup.classList.remove('popup-active')
+    }, 2500)
+});
+
+// setInterval(() =>{
+//     console.log('Tic');
+// }, 1);
+
+let counter = 0;
+const interval = setInterval(() => {
+    counter++;
+    console.log("Counter", counter);
+
+    if (counter >=5){
+        clearInterval(interval);
+        console.log("O intervalo foi removido")
+    };
+}, 1000);   
